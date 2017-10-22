d1fee5f7f45f5f06f8bd2a54ad1e2ec0ad828acb
aofleejay <aofleejay@hotmail.com>
2017-10-22 21:49:47 +0700

    create login form and event handling

--- App.js
+++ App.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View } from 'react-native'
+import firebase from 'firebase'
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  DATABASE_URL,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID
+} from 'react-native-dotenv'
 import { Header } from './src/components/common'
+import LoginForm from './src/components/LoginForm'
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: API_KEY,
+      authDomain: AUTH_DOMAIN,
+      databaseURL: DATABASE_URL,
+      projectId: PROJECT_ID,
+      storageBucket: STORAGE_BUCKET,
+      messagingSenderId: MESSAGING_SENDER_ID,
+    })
+  }
+
   render() {
     return(
       <View>
         <Header headerText="Authentication" />
-        <Text>Hello World</Text>
+        <LoginForm />
       </View>
     )
   }
